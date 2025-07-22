ade168fe3614892a3de9e2d080b245edd2da9eda
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-07-22 10:35:31 +0530

    Add files via upload

---
+++ JS/app01.js
@@ -0,0 +1,50 @@
+// window.addEventListener("load",
+//     function (){
+//         console.log("document loaded successfully");
+//         let box = document.querySelector("#box");
+//         let btn = document.querySelector(".btn");
+//         let h1 = document.querySelector("h1");
+//         let div = document.querySelector("div");
+//         btn.addEventListener(
+//             "click",
+//             // anonymous function
+//             function (){
+//                 h1.innerText =  "Good Morning " +box.value;
+//             }
+//         )
+//     }
+// )
+// // var - gloabl variable - 
+// // let - block scoped
+
+
+
+// // banana bracket
+
+
+
+
+
+
+
+
+
+        let box = document.querySelector("#box");
+        let btn = document.querySelector(".btn");
+        let h1 = document.querySelector("h1");
+        let div = document.querySelector("div");
+        btn.addEventListener(
+            "click",
+            // anonymous function
+            function (){
+                h1.innerText =  "Good Morning " +box.value;
+            }
+        )
+    
+// var - gloabl variable - 
+// let - block scoped
+
+
+
+// banana bracket
+
